2204caf435dc026380ddf21f6fecc445c550f71d
Gyoza-Gyoza <calebwjle@gmail.com>
2024-09-20 11:00:23 +0800

    missileblock WIP

--- Assets/Test/BlockV2Test/Scripts/BridgingBlock.cs
+++ Assets/Test/BlockV2Test/Scripts/BridgingBlock.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class BridgingBlock : Block
+public class BridgingBlock : ChainingBlock
 {
     [SerializeField]
     protected Color activeColor,
